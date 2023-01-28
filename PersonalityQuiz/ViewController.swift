f38ad39c8b5dcbda1ae3f007e5ecda86d5fd121b
Mohammed AlZiyad <m.alziyad911@icloud.com>
2023-01-28 23:20:36 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Mohammed AlZiyad on 06/07/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
