2afa9a9edd526eb39552ece2cff541d2b43c0c9d
Mohammed AlZiyad <113702772+Mohammed-AlZiyad@users.noreply.github.com>
2023-01-31 10:02:00 +0300

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Mohammed AlZiyad on 06/07/1444 AH.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
